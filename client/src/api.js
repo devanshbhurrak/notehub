c7906508987157ad5b2508aef29ec379b0fa14fa
Devansh Bhurrak <144364530+devanshbhurrak@users.noreply.github.com>
2025-04-26 12:18:40 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6969',
+  baseURL: 'https://notehub-backend.vercel.app/',
 });
 
 api.interceptors.request.use(config => {
